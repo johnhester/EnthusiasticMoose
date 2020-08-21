d88572828039e15a6ae39c2da3f7b389a10d0a23
John Hester <hesterusmaximus@gmail.com>
2020-08-21 10:07:10 -0500

    phase 4

--- Program.cs
+++ Program.cs
@@ -10,40 +10,41 @@ namespace EnthusiasticMoose
             Console.WriteLine("--------------------------------------------");
             Console.WriteLine();
 
-            // Let the moost speak!
-            MooseSays();
+            // Let the moose speak!
+            MooseSays("H I, I'M  E N T H U S I A S T I C !");
+            MooseSays("I really am enthusiastic");
         }
 
-        static void MooseSays()
+        static void MooseSays(string message)
         {
-            Console.WriteLine(@"
-                                    _.--^^^--,
+            Console.WriteLine($@"
+                                      _.--^^^--,
                                     .'          `\
-.-^^^^^^-.                      .'              |
-/          '.                   /            .-._/
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
 |             `.                |             |
-\              \          .-._ |          _   \
-`^^'-.         \_.-.     \   `          ( \__/
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
         |             )     '=.       .,   \
-    /             (         \     /  \  /
-    /`               `\        |   /    `'
-    '..-`\        _.-. `\ _.__/   .=.
-        |  _    / \  '.-`    `-.'  /
-        \_/ |  |   './ _     _  \.'
-            '-'    | /       \ |
-                    |  .-. .-.  |
-                    \ / o| |o \ /
-                    |   / \   |    H I, I'M  E N T H U S I A S T I C !
-                    / `^`   `^` \
-                    /             \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    {message}
+                      / `^`   `^` \
+                     /             \
                     | '._.'         \
                     |  /             |
-                    \ |             |
-                    ||    _    _   /
-                    /|\  (_\  /_) /
-                    \ \'._  ` '_.'
-                    `^^` `^^^`
-            ");
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
         }
     }
 }
